0681f6179aeab0e2ea2936103f536eda3e40aca3
root <mitvparekh9@yahoo.com>
2018-03-13 15:44:38 +0000

    Server Push

--- app/models.py
+++ app/models.py
@@ -12,6 +12,36 @@ from django.db import models
 # Table Supplier logs: id, name, quantity supplied, type(food, first aid etc)
 # Table Current stock: current food packets, current first aid packets
 
+SHELTER_TYPE_CHOICES =(
+    ('g','government'),
+    ('a','ad-hoc'),
+)
+
+GENDER_CHOICES = (
+    ('m', 'Male'),
+    ('f', 'Female'),
+)
+
+ROLE_CHOICES = (
+    ('a', 'Admin'),
+    ('o', 'Operator'),
+    ('s', 'Supplier'),
+)
+
+SUPPLY_TYPE_CHOICES = (
+    ('fp','food_packet'),
+    ('fa','first_aid'),
+    ('b','beddings'),
+    ('w','water'),
+)
+
+class CurrentStock(models.Model):
+    food_packets_available = models.IntegerField()
+    first_aid_packets_available = models.IntegerField()
+    bedding_packets_available = models.IntegerField()
+    water_available = models.IntegerField()
+
+
 class Shelter(models.Model):
     current_stock = models.ForeignKey(CurrentStock)
     total_capacity_of_people =  models.IntegerField()
@@ -22,16 +52,7 @@ class Shelter(models.Model):
     shelter_longitude = models.DecimalField()
     shelter_type = models.CharField(choices = SHELTER_TYPE_CHOICES)
 
-    SHELTER_TYPE_CHOICES =(
-        ('g','government'),
-        ('a','ad-hoc'),
-    )
-
-class CurrentStock(models.Model):
-    food_packets_available = models.IntegerField()
-    first_aid_packets_available = models.IntegerField()
-    bedding_packets_available = models.IntegerField()
-    water_available = models.IntegerField()
+    
 
 class SystemUsers(AbstractUser):
     shelter_id = models.ForeignKey(Shelter)
@@ -46,15 +67,9 @@ class SystemUsers(AbstractUser):
     aadhar_number = models.IntegerField()
     user_role = models.CharField( choices = ROLE_CHOICES)
 
-    GENDER_CHOICES = (
-        ('m', 'Male'),
-        ('f', 'Female'),
-    )
-    ROLE_CHOICES = (
-        ('a', 'Admin'),
-        ('o', 'Operator'),
-        ('s', 'Supplier'),
-    )
+class Families(models.Model):
+    last_name = last_name = models.CharField(max_length=100 )
+    number_of_members = contact = models.IntegerField()
 
 class Civilians(models.Model):
     family_id = models.ForeignKey(Families)
@@ -75,23 +90,8 @@ class Civilians(models.Model):
     blood_group = models.CharField(max_length=100)
     parent_gaurdian = models.CharField(max_length=100)
 
-    GENDER_CHOICES = (
-        ('m', 'Male'),
-        ('f', 'Female'),
-    )
-
-class Families(models.Model):
-    last_name = last_name = models.CharField(max_length=100 )
-    number_of_members = contact = models.IntegerField()
 
 class SupplierLogs(models.Model):
-    name = models.CharField(max_length=100)
+    supplier = models.ForeignKey(SystemUsers)
     quantity_supplied = models.IntegerField()
     supply_type = models.CharField( choices = SUPPLY_TYPE_CHOICES)
-
-    SUPPLY_TYPE_CHOICES = (
-        ('fp','food_packet'),
-        ('fa','first_aid'),
-        ('b','beddings'),
-        ('w','water'),
-    )
