bd27ba40f555f5bb614dffe1cba10c1e6529ca3f
root <mitvparekh9@yahoo.com>
2018-03-13 16:04:53 +0000

    Model errors fixed

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
@@ -35,27 +35,25 @@ SUPPLY_TYPE_CHOICES = (
     ('w','water'),
 )
 
+class Shelter(models.Model):
+    total_capacity_of_people = models.IntegerField()
+    capacity_used =  models.IntegerField()
+    food_packets_needed = models.IntegerField()
+    first_aid_packets_needed = models.IntegerField()
+    shelter_latitude = models.DecimalField(max_digits=9,decimal_places=6)
+    shelter_longitude = models.DecimalField(max_digits=9,decimal_places=6)
+    shelter_type = models.CharField(max_length=10,choices = SHELTER_TYPE_CHOICES)
+
+
 class CurrentStock(models.Model):
+    current_stock = models.ForeignKey(Shelter)
     food_packets_available = models.IntegerField()
     first_aid_packets_available = models.IntegerField()
     bedding_packets_available = models.IntegerField()
     water_available = models.IntegerField()
-
-
-class Shelter(models.Model):
-    current_stock = models.ForeignKey(CurrentStock)
-    total_capacity_of_people =  models.IntegerField()
-    capacity_used =  models.IntegerField()
-    food_packets_needed = models.IntegerField()
-    first_aid_packets_needed = models.IntegerField()
-    shelter_latitude = models.DecimalField()
-    shelter_longitude = models.DecimalField()
-    shelter_type = models.CharField(choices = SHELTER_TYPE_CHOICES)
-
     
-
 class SystemUsers(AbstractUser):
-    shelter_id = models.ForeignKey(Shelter)
+    shelter = models.ForeignKey(Shelter)
     first_name = models.CharField(max_length=100 )
     last_name = models.CharField(max_length=100 )
     contact = models.IntegerField()
@@ -63,18 +61,18 @@ class SystemUsers(AbstractUser):
     add_line_1 = models.CharField(max_length=100)
     add_line_2 = models.CharField(max_length=100 , blank = True , null = True)
     add_line_3 = models.CharField(max_length=100 , blank = True , null = True)
-    gender = models.CharField( choices = GENDER_CHOICES)
+    gender = models.CharField(max_length=10, choices = GENDER_CHOICES)
     aadhar_number = models.IntegerField()
-    user_role = models.CharField( choices = ROLE_CHOICES)
+    user_role = models.CharField(max_length=10, choices = ROLE_CHOICES)
 
 class Families(models.Model):
-    last_name = last_name = models.CharField(max_length=100 )
-    number_of_members = contact = models.IntegerField()
+    last_name = models.CharField(max_length=100 )
+    number_of_members = models.IntegerField()
 
 class Civilians(models.Model):
-    family_id = models.ForeignKey(Families)
-    current_shelter = models.ForeignKey(Shelter)
-    allocated_shelter = models.ForeignKey(Shelter)
+    family = models.ForeignKey(Families)
+    current_shelter = models.ForeignKey(Shelter, related_name='current_shelter')
+    allocated_shelter = models.ForeignKey(Shelter, related_name='allocated_shelter')
     first_name = models.CharField(max_length=100 )
     last_name = models.CharField(max_length=100 )
     contact = models.IntegerField()
@@ -85,7 +83,7 @@ class Civilians(models.Model):
     city = models.CharField(max_length=100)
     state = models.CharField(max_length=100)
     country = models.CharField(max_length=100)
-    gender = models.CharField(choices = GENDER_CHOICES)
+    gender = models.CharField(max_length=10,choices = GENDER_CHOICES)
     aadhar_number = models.IntegerField()
     blood_group = models.CharField(max_length=100)
     parent_gaurdian = models.CharField(max_length=100)
@@ -94,4 +92,4 @@ class Civilians(models.Model):
 class SupplierLogs(models.Model):
     supplier = models.ForeignKey(SystemUsers)
     quantity_supplied = models.IntegerField()
-    supply_type = models.CharField( choices = SUPPLY_TYPE_CHOICES)
+    supply_type = models.CharField(max_length=10, choices = SUPPLY_TYPE_CHOICES)
