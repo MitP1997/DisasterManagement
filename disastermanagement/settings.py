aaca9b33f9313a057624c90b3539b9f178523d55
MitP97 <mitvparekh9@yahoo.com>
2018-03-13 17:15:11 +0530

    Updated settings

--- disastermanagement/settings.py
+++ disastermanagement/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '(gj8%nt2=@now&sg!9z$_9i2a%17@nz+8=4ekv%94^hboqs7zc'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['139.59.63.61','localhost']
 
 
 # Application definition
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'IST'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
